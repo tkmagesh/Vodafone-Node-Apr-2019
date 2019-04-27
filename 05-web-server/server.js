bee51d87961d3aafae589e6fcedb004c1f5b6129
Magesh <tkmagesh77@gmail.com>
2019-04-27 16:20:55 +0530

    web server - part - 1

--- 05-web-server/server.js
+++ 05-web-server/server.js
@@ -1,10 +1,25 @@
-var http = require('http');
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path');
 
 var server = http.createServer(function(req /* IncomingMessage */, res /* ServerResponse */){
 
+	var resource = path.join(__dirname, req.url);
+
+	console.log(resource);
 	
-	res.write('<h1>Introduction to Node.js</h1>');
-	res.end();
+	if (!fs.existsSync(resource)){
+		res.statusCode = 404;
+		res.end();
+		return;
+	}
+
+	var stream = fs.createReadStream(resource);
+	stream.pipe(res);
 });
 
 server.listen(8080);
+
+server.on('listening', function(){
+	console.log('server listening on 8080...!');
+});

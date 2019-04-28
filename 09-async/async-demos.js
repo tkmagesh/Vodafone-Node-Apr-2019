fe2c261bf70178cb4b66045dd5bc00d3460eadc3
Magesh <tkmagesh77@gmail.com>
2019-04-28 11:00:53 +0530

    async seq

---
+++ 09-async/async-demos.js
@@ -0,0 +1,103 @@
+function f1Sync(){
+	console.log('f1Sync started');
+	console.log('f1Sync completed');
+}
+
+function f2Sync(){
+	console.log('f2Sync started');
+	console.log('f2Sync completed');
+}
+
+function f3Sync(){
+	console.log('f3Sync started');
+	console.log('f3Sync completed');
+}
+
+function f4Sync(){
+	console.log('f4Sync started');
+	console.log('f4Sync completed');
+}
+
+/*function runSync(){
+	f1Sync();
+	f2Sync();
+	f3Sync();
+	f4Sync();
+}*/
+
+var syncFns = [ f1Sync, f2Sync, f3Sync, f4Sync ];
+
+function runSync(){
+	for(var index = 0, count = syncFns.length; index < count; index++){
+		var fn = syncFns[index];
+		fn();
+	}
+}
+
+module.exports['runSync'] = runSync;
+
+function f1Async(next){
+	console.log('f1Async started');
+	setTimeout(function(){
+		console.log('f1Async completed');
+		if (typeof next === 'function')
+			next();
+	}, 3000);
+}
+
+function f2Async(next){
+	console.log('f2Async started');
+	setTimeout(function(){
+		console.log('f2Async completed');
+		if (typeof next === 'function')
+			next();
+	}, 3000);
+}
+
+function f3Async(next){
+	console.log('f3Async started');
+	setTimeout(function(){
+		console.log('f3Async completed');
+		if (typeof next === 'function')
+			next();
+	}, 3000);
+}
+
+function f4Async(next){
+	console.log('f4Async started');
+	setTimeout(function(){
+		console.log('f4Async completed');
+		if (typeof next === 'function')
+			next();
+	}, 3000);
+}
+
+/*function runAsync(){
+	//f1Async(f2Async(f3Async(f4Async)))
+	f1Async(function(){
+		f2Async(function(){
+			f3Async(function(){
+				f4Async();
+			});
+		});
+	});
+}*/
+
+var asyncFuns = [f1Async, f2Async, f3Async, f4Async];
+
+function exec(fns){
+	var first = fns[0],
+		remaining = fns.slice(1),
+		next = function(){
+			exec(remaining);
+		};
+	if (typeof first === 'function')
+		first(next);
+}
+
+function runAsync(){
+	exec(asyncFuns);
+}
+
+module.exports['runAsync'] = runAsync;
+
